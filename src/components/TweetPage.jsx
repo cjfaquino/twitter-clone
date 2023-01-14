217ae43a804e053544f609d93bc9bea24803be82
Christian Aquino <cjfaquino@gmail.com>
2023-01-13 19:57:41 -0800

    add hook for getting one tweet

--- src/components/TweetPage.jsx
+++ src/components/TweetPage.jsx
@@ -1,21 +1,39 @@
-import React from 'react';
-import { useLocation } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useLocation, useParams } from 'react-router-dom';
 import { Timestamp } from 'firebase/firestore';
 import TweetItem from './TweetItem';
+import Replies from './Replies';
+import useOneTweet from '../utils/useOneTweet';
 
 function TweetPage() {
   const location = useLocation();
-  const { tweetObj } = location.state;
-  const timestamp = new Timestamp(
-    tweetObj.timestamp.seconds,
-    tweetObj.timestamp.nanoseconds
-  );
-  const newObj = { ...tweetObj, timestamp };
+  const params = useParams();
+  const [tweet, setTweet, isTweetLoading] = useOneTweet(params.tweet);
+
+  useEffect(() => {
+    if (location.state !== null && !tweet) {
+      const { tweetObj } = location.state;
+      let newObj = tweetObj;
+
+      if (!(tweetObj.timestamp instanceof Timestamp)) {
+        // convert timestamp object back into firestore custom Timestamp object
+        const timestamp = new Timestamp(
+          tweetObj.timestamp.seconds,
+          tweetObj.timestamp.nanoseconds
+        );
+
+        // set updated tweetObj
+        newObj = { ...tweetObj, timestamp };
+      }
+
+      setTweet(newObj);
+    }
+  }, []);
 
   return (
     <div id='feed'>
       TweetPage
-      <TweetItem tweetObj={newObj} />
+      <div>{tweet && tweet.text}</div>
     </div>
   );
 }
---
+++ src/utils/useOneTweet.js
@@ -0,0 +1,25 @@
+import { useEffect, useState } from 'react';
+import { doc, getDoc } from 'firebase/firestore';
+import { db } from '../firebase-config';
+
+export default function useOneTweet(tweetID) {
+  const [tweet, setTweet] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const docRef = doc(db, 'tweets', tweetID);
+
+    setLoading(true);
+    getDoc(docRef)
+      .then((docSnap) => {
+        if (docSnap.exists() && !tweet) {
+          setTweet(docSnap.data());
+        } else {
+          // doc.data() will be undefined in this case
+          console.log('No such document!');
+        }
+      })
+      .finally(setLoading(false));
+  }, []);
+  return [tweet, setTweet, loading];
+}
