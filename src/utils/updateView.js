3e71ef5d85c9a6d186b2ab78aa0f738b0261d60b
Christian Aquino <cjfaquino@gmail.com>
2023-01-27 18:56:40 -0800

    fix issue with css selector

--- src/utils/updateView.js
+++ src/utils/updateView.js
@@ -1,7 +1,9 @@
 import increaseViewCount from './increaseViewCount';
 
 const updateView = async (tweetObj) => {
-  const viewsDOM = document.querySelector(`#${tweetObj.id} .views-number`);
+  const viewsDOM = document.querySelector(
+    `div.main-tweet-item-stats > span.views > span.views-number`
+  );
   await increaseViewCount(tweetObj);
   viewsDOM.textContent = Number(viewsDOM.textContent) + 1;
 };
