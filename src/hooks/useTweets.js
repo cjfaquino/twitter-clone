8ed4ef6c028cc5b05cb149509df85b7899afa9b8
Christian Aquino <cjfaquino@gmail.com>
2023-02-03 12:54:32 -0800

    refactor useTweets

--- src/hooks/useTweets.js
+++ src/hooks/useTweets.js
@@ -6,49 +6,44 @@ export default function useTweets(filter, userID) {
   const [tweets, setTweets] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  let count = 0;
-
-  useEffect(() => {
+  const getTweets = async () => {
     const queryRef = query(
       collection(db, 'tweets'),
       orderBy('timestamp', 'desc')
     );
 
-    count += 1;
+    const qSnap = await getDocs(queryRef);
+    qSnap.forEach((item) => {
+      if (
+        filter === 'user tweets' &&
+        userID &&
+        item.data().USER_ID === userID &&
+        item.data().aReplyTo === null
+      ) {
+        setTweets((prev) => [...prev, { id: item.id, ...item.data() }]);
+      }
 
-    if (count > 1) return undefined;
+      if (
+        filter === 'user tweets&replies' &&
+        userID &&
+        item.data().USER_ID === userID
+      ) {
+        setTweets((prev) => [...prev, { id: item.id, ...item.data() }]);
+      }
 
-    getDocs(queryRef)
-      .then((qSnap) => {
-        qSnap.forEach((item) => {
-          if (
-            filter === 'user tweets' &&
-            userID &&
-            item.data().USER_ID === userID &&
-            item.data().aReplyTo === null
-          ) {
-            setTweets((prev) => [...prev, { id: item.id, ...item.data() }]);
-          }
-
-          if (
-            filter === 'user tweets&replies' &&
-            userID &&
-            item.data().USER_ID === userID
-          ) {
-            setTweets((prev) => [...prev, { id: item.id, ...item.data() }]);
-          }
-
-          if (
-            filter === 'tweets' &&
-            item.data().aReplyTo === null &&
-            !tweets.some((twt) => twt.id === item.id)
-          )
-            setTweets((prev) => [...prev, { id: item.id, ...item.data() }]);
-        });
-      })
-      .catch((e) => console.error(e));
+      if (filter === 'tweets' && item.data().aReplyTo === null)
+        setTweets((prev) => [...prev, { id: item.id, ...item.data() }]);
+    });
 
     setLoading(false);
+  };
+
+  let ran = false;
+  useEffect(() => {
+    if (!ran) {
+      getTweets();
+      ran = true;
+    }
 
     return () => {
       setTweets([]);
