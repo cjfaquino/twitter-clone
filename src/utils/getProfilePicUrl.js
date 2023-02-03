8b092c40897b503ba866c2a4fbfc0b44a5224b84
Christian Aquino <cjfaquino@gmail.com>
2023-02-02 19:17:59 -0800

    change placeholder img & move to seperate file

---
+++ src/utils/getProfilePicUrl.js
@@ -0,0 +1,10 @@
+import { getAuth } from 'firebase/auth';
+
+// Returns the signed-in user's profile Pic URL.
+// If null, use placeholder img
+
+const getProfilePicUrl = () =>
+  getAuth().currentUser.photoURL ||
+  'https://firebasestorage.googleapis.com/v0/b/twitter-clone-a66e7.appspot.com/o/user-icon.jpg?alt=media&token=8fbc3650-f297-4cd8-a996-a84470c488b5';
+
+export default getProfilePicUrl;
