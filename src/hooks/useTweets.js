35612cf53f78914a2ef4e23eda44e59627f5946f
Christian Aquino <cjfaquino@gmail.com>
2023-02-03 23:31:22 -0800

    add likes filter

--- src/hooks/useTweets.js
+++ src/hooks/useTweets.js
@@ -1,47 +1,73 @@
 import { useEffect, useState } from 'react';
 import { collection, query, getDocs, orderBy } from 'firebase/firestore';
 import { db } from '../firebase-config';
+import getUpdatedTweetByID from '../utils/getUpdatedTweetByID';
 
 export default function useTweets(filter, userID) {
   const [tweets, setTweets] = useState([]);
   const [loading, setLoading] = useState(true);
 
   const getTweets = async () => {
-    const queryRef = query(
-      collection(db, 'tweets'),
-      orderBy('timestamp', 'desc')
-    );
+    try {
+      const queryRef = query(
+        collection(db, 'tweets'),
+        orderBy('timestamp', 'desc')
+      );
 
-    const qSnap = await getDocs(queryRef);
-    qSnap.forEach((item) => {
-      if (
-        filter === 'user tweets' &&
-        userID &&
-        item.data().USER_ID === userID &&
-        item.data().aReplyTo === null
-      ) {
-        setTweets((prev) => [...prev, { id: item.id, ...item.data() }]);
-      }
+      const qSnap = await getDocs(queryRef);
+      qSnap.forEach((item) => {
+        if (
+          filter === 'user tweets' &&
+          userID &&
+          item.data().USER_ID === userID &&
+          item.data().aReplyTo === null
+        ) {
+          setTweets((prev) => [...prev, { id: item.id, ...item.data() }]);
+        }
 
-      if (
-        filter === 'user tweets&replies' &&
-        userID &&
-        item.data().USER_ID === userID
-      ) {
-        setTweets((prev) => [...prev, { id: item.id, ...item.data() }]);
-      }
+        if (
+          filter === 'user tweets&replies' &&
+          userID &&
+          item.data().USER_ID === userID
+        ) {
+          setTweets((prev) => [...prev, { id: item.id, ...item.data() }]);
+        }
 
-      if (filter === 'tweets' && item.data().aReplyTo === null)
-        setTweets((prev) => [...prev, { id: item.id, ...item.data() }]);
-    });
+        if (filter === 'tweets' && item.data().aReplyTo === null)
+          setTweets((prev) => [...prev, { id: item.id, ...item.data() }]);
+      });
+    } catch (error) {
+      console.log(error);
+    }
 
     setLoading(false);
   };
 
+  const getLikes = async () => {
+    const likesQuery = query(
+      collection(db, 'users', userID, 'likes'),
+      orderBy('likedAt', 'desc')
+    );
+    const qSnap = await getDocs(likesQuery);
+    const queue = [];
+    qSnap.forEach(async (item) => {
+      queue.push(getUpdatedTweetByID(item.id));
+    });
+    const likedTweets = await Promise.all(queue);
+    setTweets(likedTweets);
+  };
+
   let ran = false;
   useEffect(() => {
-    if (!ran) {
+    // filter is not likes
+    if (!ran && filter !== 'user likes') {
       getTweets();
+      ran = true;
+    }
+
+    // filter is likes
+    if (!ran && filter === 'user likes') {
+      getLikes();
       ran = true;
     }
 
