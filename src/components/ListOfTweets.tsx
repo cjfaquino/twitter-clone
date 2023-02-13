c368d232cdf2fe831a5b22c040cd21ff6cdc8930
Christian Aquino <cjfaquino@gmail.com>
2023-02-12 23:46:22 -0800

    add small delay to missingText

--- src/components/ListOfTweets.tsx
+++ src/components/ListOfTweets.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useLayoutEffect, useState } from 'react';
 import { TweetObj } from '../interfaces/TweetObj';
 import TweetItem from './TweetItem';
 
@@ -16,17 +16,34 @@ const ListOfTweets = ({
   tweets,
   customClass,
   missingText,
-}: IProps & typeof defaultProps) => (
-  <div className='list'>
-    {tweets.length !== 0 ? (
-      tweets.map((reply: TweetObj) => (
-        <TweetItem key={`${customClass}-${reply.id}`} tweetObj={reply} />
-      ))
-    ) : (
-      <div className='missing'>{missingText}</div>
-    )}
-  </div>
-);
+}: IProps & typeof defaultProps) => {
+  const [empty, setEmpty] = useState(false);
+
+  useLayoutEffect(() => {
+    let timer: NodeJS.Timeout;
+    if (tweets.length === 0) {
+      timer = setTimeout(() => {
+        setEmpty(true);
+      }, 500);
+    }
+    return () => {
+      clearInterval(timer);
+      setEmpty(false);
+    };
+  }, [tweets]);
+
+  return (
+    <div className='list'>
+      {empty ? (
+        <div className='missing'>{missingText}</div>
+      ) : (
+        tweets.map((reply: TweetObj) => (
+          <TweetItem key={`${customClass}-${reply.id}`} tweetObj={reply} />
+        ))
+      )}
+    </div>
+  );
+};
 
 ListOfTweets.defaultProps = defaultProps;
 
--- src/components/ListOfUsers.tsx
+++ src/components/ListOfUsers.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useLayoutEffect, useState } from 'react';
 import ProfileSmall from './ProfileSmall';
 import ProfileMedium from './ProfileMedium';
 import { UserProfile } from '../interfaces/UserProfile';
@@ -18,18 +18,35 @@ const ListOfUsers = ({
   users,
   compact,
   missingText,
-}: IProps & typeof defaultProps) => (
-  <div className='list'>
-    {users.length === 0 && <div className='missing'>{missingText}</div>}
-    {compact
-      ? users.map((usr) => (
-          <ProfileSmall key={`profile-small-${usr.id}`} userProfile={usr} />
-        ))
-      : users.map((usr) => (
-          <ProfileMedium key={`profile-medium-${usr.id}`} userProfile={usr} />
-        ))}
-  </div>
-);
+}: IProps & typeof defaultProps) => {
+  const [empty, setEmpty] = useState(false);
+
+  useLayoutEffect(() => {
+    let timer: NodeJS.Timeout;
+    if (users.length === 0) {
+      timer = setTimeout(() => {
+        setEmpty(true);
+      }, 500);
+    }
+    return () => {
+      clearInterval(timer);
+      setEmpty(false);
+    };
+  }, [users]);
+
+  return (
+    <div className='list'>
+      {empty && <div className='missing'>{missingText}</div>}
+      {compact
+        ? users.map((usr) => (
+            <ProfileSmall key={`profile-small-${usr.id}`} userProfile={usr} />
+          ))
+        : users.map((usr) => (
+            <ProfileMedium key={`profile-medium-${usr.id}`} userProfile={usr} />
+          ))}
+    </div>
+  );
+};
 
 ListOfUsers.defaultProps = defaultProps;
 
