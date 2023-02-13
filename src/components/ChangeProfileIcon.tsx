590e6fb747881f75e03f63d5b202b713a30fa74c
Christian Aquino <cjfaquino@gmail.com>
2023-02-12 22:14:58 -0800

    style profile pic upload

--- src/components/ChangeProfileIcon.tsx
+++ src/components/ChangeProfileIcon.tsx
@@ -1,6 +1,6 @@
-import { faClose } from '@fortawesome/free-solid-svg-icons';
+import { faClose, faImage } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import getProfilePicUrl from '../utils/getProfilePicUrl';
 
 const ChangeProfileIcon = () => {
@@ -8,33 +8,40 @@ const ChangeProfileIcon = () => {
   const [selectedImage, setSelectedImage] = useState<Blob | MediaSource | null>(
     null
   );
+  const inputRef = useRef(null);
 
   const resetImg = () => {
     setPhotoURL(getProfilePicUrl());
+    (inputRef.current! as HTMLInputElement).value = '';
     setSelectedImage(null);
   };
 
   const handleImg = (e: React.ChangeEvent<HTMLInputElement>) => {
-    console.log(e.target.files[0]);
-    setSelectedImage(e.target.files[0]);
-    setPhotoURL(URL.createObjectURL(e.target.files[0]));
+    setSelectedImage(e.target.files![0]);
+    setPhotoURL(URL.createObjectURL(e.target.files![0]));
   };
 
   return (
-    <div>
+    <div className='change-profile-icon'>
       <div className='img-container large-icon'>
         <img alt='not found' width='100px' src={photoURL} />
       </div>
-      <button type='button' onClick={resetImg}>
-        <FontAwesomeIcon icon={faClose} />
-      </button>
-      <input
-        type='file'
-        name='photoURL'
-        id='photoURL'
-        onChange={handleImg}
-        accept='image/jpeg,image/png,image/webp'
-      />
+      {selectedImage && (
+        <button type='button' className='btn-cancel-img' onClick={resetImg}>
+          <FontAwesomeIcon icon={faClose} />
+        </button>
+      )}
+      <label htmlFor='photoURL' className='img-file-upload' title='Add photo'>
+        <FontAwesomeIcon icon={faImage} />
+        <input
+          ref={inputRef}
+          type='file'
+          name='photoURL'
+          id='photoURL'
+          onChange={handleImg}
+          accept='image/jpeg'
+        />
+      </label>
     </div>
   );
 };
