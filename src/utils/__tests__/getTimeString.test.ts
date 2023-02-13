1f5f09089a7bffe5ab4810bc05229006d08e1591
Christian Aquino <cjfaquino@gmail.com>
2023-02-12 20:22:53 -0800

    refactor recent time strings

---
+++ src/utils/__tests__/getTimeString.test.ts
@@ -0,0 +1,42 @@
+import { Timestamp } from 'firebase/firestore';
+import getTimeString from '../getTimeString';
+
+describe('getTimeString function', () => {
+  const mockTimestamp = (msec: number) => {
+    const mock = { toDate: () => new Date(Date.now() - msec) };
+    return mock as unknown as Timestamp;
+  };
+
+  it('should output time and date if no optional locale set', () => {
+    expect(getTimeString(mockTimestamp(0))).toBe(
+      `${new Date().toLocaleTimeString()} · ${new Date().toDateString()}`
+    );
+  });
+
+  it('should output current time and date if using a locally created Tweet item', () => {
+    expect(getTimeString({} as Timestamp)).toBe(
+      `${new Date().toLocaleTimeString()} · ${new Date().toDateString()}`
+    );
+  });
+
+  it('should output in {n}s if within a minute', () => {
+    expect(getTimeString(mockTimestamp(2000), 'localeDate')).toBe('2s');
+    expect(getTimeString(mockTimestamp(59000), 'localeDate')).toBe('59s');
+  });
+
+  it('should output in {n}m if within an hour', () => {
+    expect(getTimeString(mockTimestamp(60000), 'localeDate')).toBe('1m');
+    expect(getTimeString(mockTimestamp(3540000), 'localeDate')).toBe('59m');
+  });
+
+  it('should output in {n}h if within an hour and day', () => {
+    expect(getTimeString(mockTimestamp(3600000), 'localeDate')).toBe('1h');
+    expect(getTimeString(mockTimestamp(86399999), 'localeDate')).toBe('23h');
+  });
+
+  it('should output in be in date toLocaleString if a day passed', () => {
+    expect(getTimeString(mockTimestamp(86400000), 'localeDate')).toBe(
+      new Date(Date.now() - 86400000).toLocaleDateString()
+    );
+  });
+});
---
+++ src/utils/getTimeString.ts
@@ -0,0 +1,52 @@
+import { Timestamp } from 'firebase/firestore';
+
+const getTimeString = (timestamp: Timestamp, format?: string): string => {
+  if (!timestamp) return '';
+
+  if (!format) {
+    // format for TweetPage MainTweet & hover effect by title attribute
+    return timestamp.toDate
+      ? `${timestamp.toDate().toLocaleTimeString()} · ${timestamp
+          .toDate()
+          .toDateString()}`
+      : `${new Date().toLocaleTimeString()} · ${new Date().toDateString()}`;
+  }
+
+  if (format === 'localeDate') {
+    // format for TweetItems
+    const pastDay = timestamp.toDate
+      ? timestamp.toDate().toLocaleDateString()
+      : `${new Date().toLocaleDateString()}`;
+
+    const diff = new Date().getTime() - timestamp.toDate().getTime();
+
+    let msec = diff;
+    const hh = Math.floor(msec / 1000 / 60 / 60);
+    msec -= hh * 1000 * 60 * 60;
+    const mm = Math.floor(msec / 1000 / 60);
+    msec -= mm * 1000 * 60;
+    const ss = Math.floor(msec / 1000);
+    msec -= ss * 1000;
+
+    // 24 hrs or over return full date
+    if (hh >= 24) return pastDay;
+
+    if (hh >= 1 && hh < 24) {
+      // between 1 hr and 1 day
+      return `${hh}h`;
+    }
+
+    if (mm >= 1 && hh < 1) {
+      // between minute and hour
+      return `${mm}m`;
+    }
+
+    if (mm < 1) {
+      // under a minute
+      return `${ss}s`;
+    }
+  }
+  return '';
+};
+
+export default getTimeString;
