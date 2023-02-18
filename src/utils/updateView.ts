9e40bd924663d950119837b8699c2d1ac87c3445
Christian Aquino <cjfaquino@gmail.com>
2023-02-18 03:37:58 -0800

    refactor updateView

--- src/utils/updateView.ts
+++ src/utils/updateView.ts
@@ -1,13 +1,21 @@
+import { doc, increment, updateDoc } from 'firebase/firestore';
 import { TweetObj } from '../interfaces/TweetObj';
 import fancyNumbers from './fancyNumbers';
-import increaseViewCount from './increaseViewCount';
+import { db } from '../firebase-config';
 
 const updateView = async (tweetObj: TweetObj) => {
-  const viewsDOM = document.querySelector(
-    `div.main-tweet-item-stats > span.views > span.views-number`
-  )!;
-  await increaseViewCount(tweetObj);
-  viewsDOM.textContent = fancyNumbers(tweetObj.views + 1);
+  try {
+    const viewsDOM = document.querySelector(
+      `div.main-tweet-item-stats > span.views > span.views-number`
+    )!;
+
+    const tweetRef = doc(db, 'tweets', tweetObj.id);
+    // increase view of tweet
+    await updateDoc(tweetRef, { views: increment(1) });
+    viewsDOM.textContent = fancyNumbers(tweetObj.views + 1);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export default updateView;
