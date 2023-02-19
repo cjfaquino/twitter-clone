ebfaa2d1ad00bb51ae47874b28c8dfd96281e95e
Christian Aquino <cjfaquino@gmail.com>
2023-02-19 03:16:49 -0800

    add username to profile header

--- src/components/GoBackHeader.tsx
+++ src/components/GoBackHeader.tsx
@@ -10,19 +10,30 @@ const defaultProps = {
   targetUser: {},
 };
 
-const GoBackHeader = ({ targetUser }: IProps & typeof defaultProps) => {
+const GoBackHeader = ({ targetUser }: IProps) => {
   const navigate = useNavigate();
 
   const handleClick = () => {
-    navigate(-1);
+    if (!(targetUser?.userProfile && targetUser.doneLoading)) navigate(-1);
+
+    if (targetUser?.userProfile && targetUser.doneLoading)
+      navigate(`/${targetUser.userProfile.userName}`);
   };
 
   return (
     <header className='goback' onClick={handleClick} aria-hidden>
       <div className='goback-text'>
-        {targetUser && targetUser.doneLoading
-          ? `← ${targetUser.userProfile.displayName}`
-          : `← Tweet`}
+        {targetUser?.userProfile && targetUser.doneLoading ? (
+          <section>
+            <div>←</div>
+            <div>
+              <p className='header-display-name'>{`${targetUser.userProfile.displayName}`}</p>
+              <p className='header-username grey'>{`@${targetUser.userProfile.userName}`}</p>
+            </div>
+          </section>
+        ) : (
+          `← Tweet`
+        )}
       </div>
     </header>
   );
