7bbf8dc195569c34083c7d69fcde2a03bfb6feef
Christian Aquino <cjfaquino@gmail.com>
2023-02-20 17:19:03 -0800

    refactor to be locale friendly

--- src/utils/__tests__/fancyNumbers.test.ts
+++ src/utils/__tests__/fancyNumbers.test.ts
@@ -12,25 +12,25 @@ describe('fancyNumbers a number formatter', () => {
   });
 
   it('should output numbers below 5 digits to show value as string', () => {
-    expect(fancyNumbers(9999)).toBe('9,999');
-    expect(fancyNumbers(5000)).toBe('5,000');
+    expect(fancyNumbers(9900)).toBe('9.9K');
+    expect(fancyNumbers(5000)).toBe('5K');
     expect(fancyNumbers(300)).toBe('300');
   });
 
   it('should shorten numbers 5 digits or more', () => {
     expect(fancyNumbers(12345)).toBe('12.3K');
-    expect(fancyNumbers(123456)).toBe('123.4K');
+    expect(fancyNumbers(123456)).toBe('123.5K');
   });
 
   it('should shorten even more for numbers 7 digits or more', () => {
     expect(fancyNumbers(1234567)).toBe('1.2M');
     expect(fancyNumbers(12345678)).toBe('12.3M');
-    expect(fancyNumbers(123456789)).toBe('123.4M');
+    expect(fancyNumbers(123456789)).toBe('123.5M');
   });
 
   it('should shorten even more for numbers 10 digits or more', () => {
     expect(fancyNumbers(1234567890)).toBe('1.2B');
     expect(fancyNumbers(12345678901)).toBe('12.3B');
-    expect(fancyNumbers(123456789012)).toBe('123.4B');
+    expect(fancyNumbers(123456789012)).toBe('123.5B');
   });
 });
--- src/utils/formatters/fancyNumbers.ts
+++ src/utils/formatters/fancyNumbers.ts
@@ -2,23 +2,10 @@ export default (number?: number, returnZero?: boolean): string => {
   if (number === undefined || (!returnZero && number < 1)) return '';
   if (returnZero && number < 1) return '0';
 
-  let formatted = new Intl.NumberFormat('en-us').format(number);
-  const { length } = number.toString();
-
-  if (length >= 5) {
-    formatted = `${formatted.replace(',', '.').slice(0, -2)}`;
-    formatted += 'K';
-  }
-
-  if (length >= 7) {
-    formatted = formatted.slice(0, -5);
-    formatted += 'M';
-  }
-
-  if (length >= 10) {
-    formatted = formatted.slice(0, -5);
-    formatted += 'B';
-  }
+  const formatted = new Intl.NumberFormat(undefined, {
+    notation: 'compact',
+    maximumFractionDigits: 1,
+  }).format(number);
 
   return formatted;
 };
